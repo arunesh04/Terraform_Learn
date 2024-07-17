dbd38631a7723597bdb9ceb6f98e2a14bcdc9776
arunesh04 <aruneshj07@gmail.com>
2024-07-17 17:22:40 +0530

    deployment

---
+++ Kubernetes/modules/deployments/nginx-deployment.tf
@@ -0,0 +1,45 @@
+resource "kubernetes_deployment" "nginx" {
+  metadata {
+    name = "nginx-deployment"
+    labels = {
+      App = "Nginx"
+    }
+  }
+
+  spec {
+    replicas = 2
+    selector {
+      match_labels = {
+        App = "Nginx"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          App = "Nginx"
+        }
+      }
+      spec {
+        container {
+          image = var.image
+          name  = "nginx"
+
+          port {
+            container_port = 80
+          }
+
+          resources {
+            limits = {
+              cpu    = "0.5"
+              memory = "512Mi"
+            }
+            requests = {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
+          }
+        }
+      }
+    }
+  }
+}
